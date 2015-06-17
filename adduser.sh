abd2b4055ea10d32df78dae21793204b7b1a0e44
rafael859 <rafael859@gmail.com>
2015-06-17 17:25:27 +0300

    Removed debug commands

--- adduser.sh
+++ adduser.sh
@@ -6,7 +6,5 @@ else
         salt=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9./' | fold -w 16 | head -n 1)
         userhash=$(echo -n $1 | sha256sum | sed 's/  -//g')
         hash=$(echo -n $salt$userhash$2 | sha256sum | sed 's/  -//g')
-        echo "concat is " $userhash$2
-        echo "hash is " $hash
         echo $salt:$hash >> /usr/share/duress/hashes
 fi
